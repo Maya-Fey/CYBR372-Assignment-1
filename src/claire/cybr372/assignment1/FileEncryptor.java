d9773493e6effc722a9b1b3836f6ae8e6242ffad
Claire <foxcoindev@gmail.com>
2020-09-03 14:07:01 +1200

    Added given FileEncryptor class

---
+++ src/claire/cybr372/assignment1/FileEncryptor.java
@@ -0,0 +1,136 @@
+package claire.cybr372.assignment1;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.crypto.Cipher;
+import javax.crypto.CipherInputStream;
+import javax.crypto.CipherOutputStream;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+
+/**
+ *
+ * @author Erik Costlow
+ */
+public class FileEncryptor {
+    private static final Logger LOG = Logger.getLogger(FileEncryptor.class.getSimpleName());
+
+    private static final String ALGORITHM = "AES";
+    private static final String CIPHER = "AES/CBC/PKCS5PADDING";
+
+    public static void main(String[] args) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, InvalidAlgorithmParameterException, IOException {
+        
+        //This snippet is literally copied from SymmetrixExample
+        SecureRandom sr = new SecureRandom();
+        byte[] key = new byte[16];
+        sr.nextBytes(key); // 128 bit key
+        byte[] initVector = new byte[16];
+        sr.nextBytes(initVector); // 16 bytes IV
+        System.out.println("Random key=" + new String(Util.toHex(key)));
+        System.out.println("initVector=" + new String(Util.toHex(initVector)));
+        IvParameterSpec iv = new IvParameterSpec(initVector);
+        SecretKeySpec skeySpec = new SecretKeySpec(key, ALGORITHM);
+        Cipher cipher = Cipher.getInstance(CIPHER);
+        cipher.init(Cipher.ENCRYPT_MODE, skeySpec, iv);
+
+        //Look for files here
+        final Path tempDir = Files.createTempDirectory("packt-crypto");
+        
+        final Path encryptedPath = tempDir.resolve("1 - Encrypting and Decrypting files.pptx.encrypted");
+        try (InputStream fin = FileEncryptor.class.getResourceAsStream("1 - Encrypting and Decrypting files.pptx");
+                OutputStream fout = Files.newOutputStream(encryptedPath);
+                CipherOutputStream cipherOut = new CipherOutputStream(fout, cipher) {
+        }) {
+            final byte[] bytes = new byte[1024];
+            for(int length=fin.read(bytes); length!=-1; length = fin.read(bytes)){
+                cipherOut.write(bytes, 0, length);
+            }
+        } catch (IOException e) {
+            LOG.log(Level.INFO, "Unable to encrypt", e);
+        }
+        
+        LOG.info("Encryption finished, saved at " + encryptedPath);
+        
+        cipher.init(Cipher.DECRYPT_MODE, skeySpec, iv);
+        final Path decryptedPath = tempDir.resolve("1 - Encrypting and Decrypting files_decrypted.pptx");
+        try(InputStream encryptedData = Files.newInputStream(encryptedPath);
+                CipherInputStream decryptStream = new CipherInputStream(encryptedData, cipher);
+                OutputStream decryptedOut = Files.newOutputStream(decryptedPath)){
+            final byte[] bytes = new byte[1024];
+            for(int length=decryptStream.read(bytes); length!=-1; length = decryptStream.read(bytes)){
+                decryptedOut.write(bytes, 0, length);
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(FileEncryptor.class.getName()).log(Level.SEVERE, "Unable to decrypt", ex);
+        }
+        
+        LOG.info("Decryption complete, open " + decryptedPath);
+    }
+    
+    private static final class Util {
+    	
+    	private static final char[] HEX = new char[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
+    	
+    	/**
+    	 * Converts a series of bytes into hexademical representation 
+    	 * @param bytes The byte array to convert
+    	 * @return An array of chars, each representing one nibble/hexadecimal character. Hex
+    	 * will be in upper case [0-9A-F]
+    	 */
+    	public static final char[] toHex(byte[] bytes)
+    	{
+    		char[] chars = new char[bytes.length * 2];
+    		for(int i = 0; i < bytes.length; i++) {
+    			chars[i * 2 + 0] = HEX[(bytes[i] & 0xF0) >>> 4]; 
+    			chars[i * 2 + 1] = HEX[(bytes[i] & 0x0F)]; 
+    		}
+    		return chars;
+    	}
+    	
+    	/**
+    	 * @param c A hexadecimal character of any case, in range [0-9A-Fa-f]
+    	 * @return A nibble 
+    	 */
+    	private static final byte charToNibble(char c)
+    	{
+    		if(c >= '0' && c <= '9') 
+    			return (byte) (c - '0');
+    		else if(c >= 'A' && c <= 'F')
+    			return (byte) (10 + (c - 'A'));
+    		else if(c >= 'a' && c <= 'f')
+    			return (byte) (10 + (c - 'a'));
+    		else
+    			throw new IllegalArgumentException("Hexadecimal characters are between 0-9, A-F, or a-f");
+    	}
+    	
+    	/**
+    	 * Converts a hexadecimal string into bytes
+    	 * @param str The hexadecimal string as an array of characters, in any case
+    	 * @return An array of bytes
+    	 */
+    	public static final byte[] fromHex(char[] str)
+    	{
+    		if((str.length & 1) == 1) 
+    			throw new IllegalArgumentException("A hex string must have an even number of characters. Consier adding a leading zero.");
+    		byte[] nBytes = new byte[str.length / 2];
+    		for(int i = 0; i < nBytes.length; i++) {
+    			byte b = charToNibble(str[i * 2]);
+    			b <<= 4;
+    			b += charToNibble(str[i * 2 + 1]);
+    			nBytes[i] = b;
+    		}
+    		return nBytes;
+    	}
+    	
+    }
+}
